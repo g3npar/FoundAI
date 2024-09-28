b0a7e5339323ce8e780cbf80bce713aa969011ea
Parin Raizada <parinr@umich.edu>
2024-09-28 14:39:57 -0400

    env setup

---
+++ FoundAI/__init__.py
@@ -0,0 +1,20 @@
+"""FoundAI package initializer."""
+import flask
+
+# app is a single object used by all the code modules in this package
+app = flask.Flask(__name__)  # pylint: disable=invalid-name
+
+# Read settings from config module (insta485/config.py)
+app.config.from_object('FoundAI.config')
+
+# Overlay settings read from a Python file whose path is set in the environment
+# variable. Setting this environment variable is optional.
+# Docs: http://flask.pocoo.org/docs/latest/config/
+# app.config.from_envvar('INSTA485_SETTINGS', silent=True)
+
+# Tell our app about views and model.  This is dangerously close to a
+# circular import, which is naughty, but Flask was designed that way.
+# (Reference http://flask.pocoo.org/docs/patterns/packages/)  We're
+# going to tell pylint and pycodestyle to ignore this coding style violation.
+import FoundAI.views  # noqa: E402  pylint: disable=wrong-import-position
+import FoundAI.model  # noqa: E402  pylint: disable=wrong-import-position
---
+++ FoundAI/config.py
@@ -0,0 +1,19 @@
+"""Insta485 development configuration."""
+
+import pathlib
+
+# Root of this application, useful if it doesn't occupy an entire domain
+APPLICATION_ROOT = '/'
+
+# Secret key for encrypting cookies
+SECRET_KEY = b'\xbd\x1d<\x02Z\x02[\x94*\xbf\x94\xe1~9\x14\x9al\x0e\x9aJvU\xb3)'
+SESSION_COOKIE_NAME = 'login'
+
+# File Upload to var/uploads/
+FOUNDAI_ROOT = pathlib.Path(__file__).resolve().parent.parent
+UPLOAD_FOLDER = FOUNDAI_ROOT/'var'/'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+# Database file is var/insta485.sqlite3
+DATABASE_FILENAME = FOUNDAI_ROOT/'var'/'FoundAI.sqlite3'
